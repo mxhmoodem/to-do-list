9614d324233eaf4c580f340fc6b0a342752768a8
Mahmood El-Mahalawy <142519933+mxhmoodem@users.noreply.github.com>
2025-01-28 16:05:30 +0000

    Update app.js to check/uncheck and delete tasks

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const listContainer = document.getElementById('list-container');
 function addTask() {
     if(inputBox.value === '') {
         alert("Please enter a task");
-    } else {
+    } 
+    else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li)
@@ -14,3 +15,12 @@ function addTask() {
     }
     inputBox.value = '';
 }
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    } 
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
